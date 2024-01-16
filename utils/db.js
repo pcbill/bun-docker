23ed32b837d6f53610068d73a870360fade14195
Bill SJ Wu <bill_sj_wu@wistron.com>
2024-01-17 00:18:58 +0800

    remove some unnecessary code

--- utils/db.js
+++ utils/db.js
@@ -2,11 +2,20 @@ import pg from "pg";
 
 const client = new pg.Client({
     user: 'npust',
-    host: 'dpg-ceopn5da4991ihh644p0-a',
+    host: 'dpg-ceopn5da4991ihh644p0-a.oregon-postgres.render.com',
     database: 'mydb_qyjo',
     password: 'da1SVS3RLaXI8LTmsrhZBFnOZRqa8xXA',
     port: 5432, // Default PostgreSQL port
+    ssl: true
 })
+
+// const client = new pg.Client({
+//     user: '',
+//     host: 'localhost',
+//     database: 'postgres',
+//     password: '',
+//     port: 5432, // Default PostgreSQL port
+// })
 
 client.connect()
 
