3cb2683d2e50fb854a7bf011d5f92e08a1375898
Shin Jan Wu <pcbill@gmail.com>
2024-01-19 06:56:35 +0800

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ const client = new pg.Client({
     user: 'npust',
     host: 'dpg-ceopn5da4991ihh644p0-a.oregon-postgres.render.com',
     database: 'mydb_qyjo',
-    password: 'da1SVS3RLaXI8LTmsrhZBFnOZRqa8xXA',
+    password: 'da1SVS3RLaXI8LTmsrhZBFnOZRqa8xXa',
     port: 5432, // Default PostgreSQL port
     ssl: true
 })
